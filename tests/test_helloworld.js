7289d3a2fc9a6348b7c1ac47bf4da79319da2f3d
Martijn Jacobs <maerteijn@gmail.com>
2021-11-11 18:37:51 +0100

    Added a test for updating a property for the element

--- tests/test_helloworld.js
+++ tests/test_helloworld.js
@@ -1,6 +1,8 @@
 import { assert } from "./utils.js"
 
 import "../src/js/components/helloworld.js"
+import { processDOM } from "slim-js"
+import "slim-js/property-directive"
 
 describe("HelloWorld Component", () => {
   it("The Helloworld component renders the name properly", () => {
@@ -16,6 +18,23 @@ describe("HelloWorld Component", () => {
       )
       element.myName = "My adjusted name!"
       assert.equal(h1.firstChild.textContent, "Hello My adjusted name!")
+    })
+  })
+
+  it("The Helloworld component name can be updated with a property", () => {
+    const div = document.createElement("div")
+    const data = {
+      parentName: "I'm the parent",
+    }
+    div.innerHTML = /*html*/ `<hello-world .my-name="{{this.parentName}}"></hello-world>`
+
+    return customElements.whenDefined("hello-world").then(() => {
+      const { flush } = processDOM(data, div)
+      flush()
+      assert.equal(div.innerHTML, "<hello-world></hello-world>")
+
+      const element = div.querySelector("hello-world")
+      assert.equal(element.myName, data.parentName)
     })
   })
 })
